ff49f004eb213a37df88a3f52f9a75ab2a2cfd2e
Daniel <delatorrechapell@gmail.com>
2022-12-17 17:17:56 -0600

    books add to HTML correctly

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-let myLibrary = [];
+let myLibrary = []; //Array to store all books
+let bookIndex = 0; //Global variable to differentiate books in library by index
 
 function Book(title, author, pages, read) {
+  //Book object constructor
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -18,29 +20,18 @@ function Book(title, author, pages, read) {
   };
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
-  myLibrary.forEach((element) => {
-    if (element.inLibrary !== true) {
-      let div = document.createElement("div");
-      div.classList.add("books");
-      div.textContent = element.info();
-      document.getElementById("library").appendChild(div);
-      element.inLibrary = true;
-    }
-  });
-}
-
 function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
 
 function closeForm() {
-  // document.getElementById("myForm").style.display = "none";
+  document.getElementById("myForm").style.display = "none";
   setBookAttributes();
 }
 
 function setBookAttributes() {
+  //Creates new book with form values as it's corresponding properties
+  //Then calls function to add the new book to the library
   let oForm = document.forms[0];
   let book = new Book();
   book.author = oForm.elements["author"].value;
@@ -49,3 +40,30 @@ function setBookAttributes() {
   book.read = oForm.elements["read"].value;
   addBookToLibrary(book);
 }
+
+function addBookToLibrary(book) {
+  //Pushes new book to myLibrary and adds it to the HTML
+  myLibrary.push(book);
+  myLibrary.forEach((element) => {
+    if (element.inLibrary !== true) {
+      let newBook = createBookMarkup(element);
+      document //Inserts the newBook in the HTML
+        .getElementById("library")
+        .insertAdjacentHTML("beforeend", newBook);
+      element.inLibrary = true; //Adds propertie to know that the book is now in myLibrary
+      element.index = bookIndex; //Adds propertie to know the book index in myLibrary
+      bookIndex++;
+    }
+  });
+}
+
+function createBookMarkup(newBook) {
+  //Creates the HTML structure for each new Book
+  let markup = `<div class="books"> ${newBook.info()} </div>
+                <div class="buttons>
+                  <button type="submit" class="remove-button" onclick="removeBook()">Remove</button>
+                </div>`;
+  return markup;
+}
+
+function removeBook() {}
